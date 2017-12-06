8c973e3659cbf2effb772de505231cc9ce4119df
Johan Eriksson <jed.johan@gmail.com>
2017-12-06 11:45:15 +0100

    added durable timer

--- AzureFunctions/durashoppingcart/durashoppingcart/OrchestrationFunc/CartOrchestrator.cs
+++ AzureFunctions/durashoppingcart/durashoppingcart/OrchestrationFunc/CartOrchestrator.cs
@@ -3,8 +3,10 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace durashoppingcart
@@ -15,13 +17,16 @@ namespace durashoppingcart
         public static async Task<List<CartEventData>> Run([OrchestrationTrigger]DurableOrchestrationContext context, TraceWriter log)
         {
             var cartList = context.GetInput<List<CartEventData>>() ?? new List<CartEventData>();
+            var cts = new CancellationTokenSource();
+            DateTime deadline = context.CurrentUtcDateTime.Add(TimeSpan.FromMinutes(Convert.ToDouble(ConfigurationManager.AppSettings["notifiertimeout-min"])));
 
+            var notifyTask = context.CreateTimer(deadline, cts.Token);
             var addItemTask = context.WaitForExternalEvent<CartEventData>(CartEvents.AddItem);
             var removeItemTask = context.WaitForExternalEvent<CartEventData>(CartEvents.RemoveItem);
             var clearCartTask = context.WaitForExternalEvent<bool>(CartEvents.ClearCart);
-            var isCompletedTask = context.WaitForExternalEvent<bool>(CartEvents.IsCompleted);
+            var isCompletedTask = context.WaitForExternalEvent<CompleteCartEventData>(CartEvents.IsCompleted);
 
-            var resultingEvent = await Task.WhenAny(addItemTask, removeItemTask, isCompletedTask, clearCartTask);
+            var resultingEvent = await Task.WhenAny(addItemTask, removeItemTask, isCompletedTask, clearCartTask, notifyTask);
 
             if (resultingEvent == addItemTask)
             {
@@ -39,8 +44,18 @@ namespace durashoppingcart
                 log.Info($"Shopping Cart cleared.");
             }
 
-            if (resultingEvent == isCompletedTask && isCompletedTask.Result)
+            else if (resultingEvent == notifyTask)
             {
+                await context.CallActivityAsync("NotifyUser", $"You have {cartList.Count} items in your cart");
+                log.Info($"Shopping Cart Notification sent.");
+            }
+
+            if (resultingEvent == isCompletedTask)
+            {
+                if(notifyTask.Status == TaskStatus.Running)
+                {
+                    cts.Cancel();
+                }
                 log.Info("Completed updating the Shopping Cart.");
             }
             else
@@ -49,6 +64,13 @@ namespace durashoppingcart
             }
 
             return cartList;
+        }
+
+        [FunctionName("NotifyUser")]
+        public static string NotifyUser([ActivityTrigger] string name)
+        {
+            // push to eventgrid
+            return $"Hello {name}!";
         }
     }
 }
