e321ce9d3cab50b51b6725fd3c03d053efbfd25d
Johan Eriksson <jed.johan@gmail.com>
2017-12-06 14:56:35 +0100

    added 2fa via SMS (twilio) function

---
+++ AzureFunctions/durashopmfa/durashopmfa/HttpStart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace durashopmfa
+{
+    public static class HttpStart
+    {
+        [FunctionName("HttpStart")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, methods: "post", Route = "orchestrators/{functionName}")] HttpRequestMessage req, [OrchestrationClient] DurableOrchestrationClient starter, string functionName, TraceWriter log)
+        {
+            dynamic eventData = await req.Content.ReadAsAsync<object>();
+            string instanceId = await starter.StartNewAsync(functionName, eventData);
+
+            log.Info($"Started orchestration with ID = '{instanceId}'.");
+
+            var res = starter.CreateCheckStatusResponse(req, instanceId);
+            res.Headers.RetryAfter = new RetryConditionHeaderValue(TimeSpan.FromSeconds(10));
+            return res;
+        }
+    }
+}
