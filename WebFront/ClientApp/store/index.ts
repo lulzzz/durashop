3f88457a2d019f780ca04cc5dc6bc291cb3c3191
Robin Nord <robin_nord_2@hotmail.com>
2017-12-07 17:11:25 +0100

    x

--- WebFront/ClientApp/store/index.ts
+++ WebFront/ClientApp/store/index.ts
@@ -1,10 +1,12 @@
 import * as WeatherForecasts from './WeatherForecasts';
 import * as ProductStore from './ProductStore';
+import * as PhoneNumberStore from './PhoneNumberStore';
 
 // The top-level state object
 export interface ApplicationState {
     product: ProductStore.ProductState;
     weatherForecasts: WeatherForecasts.WeatherForecastsState;
+    phoneNumberStore: PhoneNumberStore.PhoneNumberState;
 }
 
 // Whenever an action is dispatched, Redux will update each top-level application state property using
@@ -12,7 +14,8 @@ export interface ApplicationState {
 // acts on the corresponding ApplicationState property type.
 export const reducers = {
     product: ProductStore.reducer,
-    weatherForecasts: WeatherForecasts.reducer
+    weatherForecasts: WeatherForecasts.reducer,
+    phoneNumberStore: PhoneNumberStore.reducer
 };
 
 // This type can be used as a hint on action creators so that its 'dispatch' and 'getState' params are
