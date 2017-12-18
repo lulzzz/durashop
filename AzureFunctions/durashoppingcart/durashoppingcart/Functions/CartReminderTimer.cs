c3750b746c5fe50068590171c3078aa253f67b90
Johan Eriksson <jed.johan@gmail.com>
2017-12-18 22:12:39 +0100

    Included Event Grid post

--- AzureFunctions/durashoppingcart/durashoppingcart/Functions/CartReminderTimer.cs
+++ AzureFunctions/durashoppingcart/durashoppingcart/Functions/CartReminderTimer.cs
@@ -1,4 +1,5 @@
 ﻿using durashoppingcart.Models;
+using durashoppingcart.Utils;
 using Microsoft.Azure.WebJobs;
 using System;
 using System.Net.Http;
@@ -19,19 +20,19 @@ namespace durashoppingcart.Functions
             var notifiyTime = context.CurrentUtcDateTime.Add(TimeSpan.FromMinutes(Convert.ToDouble(remData.RemindInMinutes)));
             await context.CreateTimer(notifiyTime, CancellationToken.None);
 
-            // Need to query the cart
+            // Query the cart
             using (var httpClient = new HttpClient())
             {
                 var response = httpClient.GetAsync(new Uri(remData.CartUrl)).Result;
                 cInstance = await response.Content.ReadAsAsync<CartInstance>();
             }
 
-            // Only act if the cart is in status "Running" AND it contains > 0 items
-            if ((cInstance.input.Count > 1) && (cInstance.runtimeStatus == "Running"))
+            // Only notify if the cart is in status "Running" AND it contains > 0 items
+            if ((cInstance.input.Count > 0) && (cInstance.runtimeStatus == "Running"))
             {
-                var ett = 0;
-                ett++;
                 // Push notif to Event Grid (mail/SMS or whatever)
+                EventGridReminder.Add(cInstance);
+
             }
         }
     }
---
+++ AzureFunctions/durashoppingcart/durashoppingcart/Utils/EventGridReminder.cs
@@ -0,0 +1,50 @@
+﻿using durashoppingcart.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace durashoppingcart.Utils
+{
+    public static class EventGridReminder
+    {
+        static string topicEndpoint = "https://durashop-cartreminder.westus2-1.eventgrid.azure.net/api/events";
+        static string sasKey = "oF1lQoeDI/H/AmVlDTPMqjKye5jafkO8Zga3tM8vuwM=";
+
+        public static async void Add(CartInstance ci)
+        {
+            List<GridEvent<object>> eventList = new List<GridEvent<object>>();
+            GridEvent<object> eventCartReminder = new GridEvent<object>
+            {
+                Subject = $"CART.REMINDER",
+                EventType = $"REMINDER.ITEMSINCART",
+                EventTime = DateTime.UtcNow,
+                Id = ci.input.FirstOrDefault().CartId,
+                Data = ci
+            };
+
+            eventList.Add(eventCartReminder);
+            await PostToEventGridAsync(eventList);
+        }
+
+        private static async Task PostToEventGridAsync(List<GridEvent<object>> data)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("aeg-sas-key", sasKey);
+                client.DefaultRequestHeaders.UserAgent.ParseAdd("DuraShoppingCartReminder");
+
+                string json = JsonConvert.SerializeObject(data);
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, topicEndpoint)
+                {
+                    Content = new StringContent(json, Encoding.UTF8, "application/json")
+                };
+
+                HttpResponseMessage response = await client.SendAsync(request);
+            }
+        }
+    }
+}
