b5297c5f8032f3eef32865e29035105f477098a7
Johan Eriksson <jed.johan@gmail.com>
2018-05-16 14:59:22 +0200

    Removed server polling

--- AzureFunctions/durashoppingcart/durashoppingcart/HttpStartCart.cs
+++ AzureFunctions/durashoppingcart/durashoppingcart/HttpStartCart.cs
@@ -1,12 +1,7 @@
-﻿using durashoppingcart.Models;
-using durashoppingcart.Polling;
-using durashoppingcart.Utils;
-using Microsoft.Azure.WebJobs;
+﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
-using Newtonsoft.Json;
 using System;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -18,7 +13,9 @@ namespace durashoppingcart
         private const string RetryInterval = "retryInterval";
 
         [FunctionName("HttpStartCart")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "{functionName}")]HttpRequestMessage req, [OrchestrationClient]DurableOrchestrationClientBase orchestrationClient, string functionName, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, 
+            "post", Route = "{functionName}")]HttpRequestMessage req, 
+            [OrchestrationClient]DurableOrchestrationClientBase orchestrationClient, string functionName, TraceWriter log)
         {
             dynamic eventData = await req.Content.ReadAsAsync<object>();
             string instanceId = await orchestrationClient.StartNewAsync(functionName, eventData);
@@ -33,15 +30,6 @@ namespace durashoppingcart
                 instanceId,
                 timeout,
                 retryInterval).ConfigureAwait(false);
-
-            //var orchStatus = orchestrationClient.CreateCheckStatusResponse(req, instanceId);
-            //var clientResponse = JsonConvert.DeserializeObject<OrchestrationClientResponse>(await orchStatus.Content.ReadAsStringAsync());
-            //var executionDetails = Helper.GetExecutionDetails(RequestMethod.StartCart);
-            //await ProvideOutput.DoIt(clientResponse, executionDetails, log);
-
-            //return orchestrationClient.CreateCheckStatusResponse(req, instanceId);
-
-
         }
 
         private static TimeSpan? GetTimeSpan(HttpRequestMessage request, string queryParameterName)
